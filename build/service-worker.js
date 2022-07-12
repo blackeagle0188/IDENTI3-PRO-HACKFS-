4090fcfbd8caacbdaf87bd36e9a7d1d6bb74be3d
Lyons800 <oisinlyons13@gmail.com>
2022-07-12 21:04:49 +0100

    Commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/volt-pro-react/precache-manifest.7f8a505082ec0ecb474f4013219501c2.js"
+  "/volt-pro-react/precache-manifest.ac5048b34dc5538b3e6b048edc313809.js"
 );
 
 self.addEventListener('message', (event) => {
